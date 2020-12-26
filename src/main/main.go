88ebfdbb09bff3b3dfe3c0197c95bdf41fe17209
VladimirKorennoy <vladimir.korennoy@gmail.com>
2020-12-26 17:38:44 +0300

    handlebars helpers done

--- src/main/main.go
+++ src/main/main.go
@@ -708,8 +708,155 @@ func registerAllHelpers(template *raymond.Template)  {
 		return fmt.Sprintf("%s:%s", min, sec);
 	})
 
+	template.RegisterHelper("dashHelper", func(x string) string {
+		if x == "0" || x == "" {
+			return "-"
+		}
+		return x
+	})
+
+	template.RegisterHelper("pressureHelper", func(pressure string, upper string) string {
+		if pressure != "" && upper != "" && upper != "0" {
+			return pressure
+		}
+		return "-"
+	})
+
+	template.RegisterHelper("allowHelper", func(allow string) string {
+		if allow == "Допущен" {
+			return "Прошел"
+		}
+		return "Не прошел"
+	})
+
+	template.RegisterHelper("upper", func(str string) string {
+		return strings.ToUpper(str)
+	})
+
+	template.RegisterHelper("ifnull", func(obj interface{}, ifNull interface{}) interface{} {
+		if obj == nil {
+			return ifNull
+		} else {
+			return obj
+		}
+	})
+
+	template.RegisterHelper("isAfterBeforeSheet", func(sheetName string) bool {
+		return strings.Contains(sheetName, "Предрейсовый") || strings.Contains(sheetName, "Послерейсовый") || strings.Contains(sheetName, "Линейный")
+	})
+
+	template.RegisterHelper("summarize", func(obj map[string]interface{}) interface{} {
+		sheetNameInterface := obj["sheetName"]
+		sheetName := fmt.Sprintf("%v", sheetNameInterface)
+
+		if strings.Contains(sheetName, "Предрейсовый") {
+			return beforeSumRows(obj)
+		}
+
+		if strings.Contains(sheetName, "Послерейсовый") {
+			return afterSumRows(obj)
+		} else {
+			return ""
+		}
+	})
+
+	template.RegisterHelper("lineSumRows", func(obj map[string]interface{}) interface{} {
+		inspectionsList := obj["inspections"].([]interface{})
+		allCount := len(inspectionsList)
+		goodLine := 0
+		badLine := 0
+
+		for _,inspection := range inspectionsList {
+			inspAllow := inspection.(map[string] interface{})["allow"].(string)
+
+			if inspAllow == "Допущен" {
+				goodLine++
+			} else {
+				badLine++
+			}
+		}
+
+		return fmt.Sprintf(`<tr style="height: 300px; border-style: solid"><td colspan="4" style="text-align: left">Итого осмотрено: </td><td></td><td></td><td></td><td>%d</td></tr>
+    			<tr style="height: 300px; border-style: solid"><td colspan="4" style="text-align: left">Итого прошло линейный контроль: </td><td></td><td></td><td></td><td>%d</td></tr>
+    			<tr style="height: 300px; border-style: solid"><td colspan="4" style="text-align: left">Итого отстраненных от трудовых обязанностей: </td><td></td><td></td><td></td><td>%d</td></tr>`,
+				allCount, goodLine, badLine)
+	})
+
+	template.RegisterHelper("faceIdNotFoundName", func(name string, surname string, patronymic string) string {
+		if name != "" {
+			return fmt.Sprintf("%s %s %s", surname, name, patronymic)
+		}
+		return "нет соответствия"
+	})
 
 
 }
 
+func beforeSumRows(obj map[string]interface{}) interface{} {
+	inspectionsList := obj["inspections"].([]interface{})
+	allCount := len(inspectionsList)
+	goodBefore := 0
+	badBefore := 0
+	goodLine := 0
+	badLine := 0
+
+	for _,inspection := range inspectionsList {
+		inspType := inspection.(map[string] interface{})["type"].(string)
+		inspAllow := inspection.(map[string] interface{})["allow"].(string)
+
+		if inspType == "Предрейсовый" || inspType == "Предсменный" {
+			if inspAllow == "Допущен" {
+				goodBefore++
+			} else {
+				badBefore++
+			}
+		} else {
+			if inspAllow == "Допущен" {
+				goodLine++
+			} else {
+				badLine++
+			}
+		}
+	}
+
+	return fmt.Sprintf(`<tr style="height: 300px; border-style: solid;"><td colspan="4" style="text-align: left">Итого осмотрено: </td><td></td><td></td><td></td><td>%d</td></tr>
+		<tr style="height: 300px; border-style: solid;"><td colspan="4" style="text-align: left">Итого допущено к исполнению трудовых обязанностей: </td><td></td><td></td><td></td><td>%d</td></tr>
+		<tr style="height: 300px; border-style: solid;"><td colspan="4" style="text-align: left">Итого не допущено к исполнению трудовых обязанностей: </td><td></td><td></td><td></td><td>%d</td></tr>`,
+		allCount, goodBefore, badBefore)
+}
+
+func afterSumRows(obj map[string]interface{}) interface{} {
+	inspectionsList := obj["inspections"].([]interface{})
+	allCount := len(inspectionsList)
+	goodAfter := 0
+	badAfter := 0
+	goodAftershift := 0
+	badAftershift := 0
+
+	for _,inspection := range inspectionsList {
+		inspType := inspection.(map[string] interface{})["type"].(string)
+		inspAllow := inspection.(map[string] interface{})["allow"].(string)
+
+		if inspType == "Послерейсовый" {
+			if inspAllow == "Допущен" {
+				goodAfter++
+			} else {
+				badAfter++
+			}
+		} else {
+			if inspAllow == "Допущен" {
+				goodAftershift++
+			} else {
+				badAftershift++
+			}
+		}
+	}
+
+	return fmt.Sprintf(`<tr style="height: 300px; border-style: solid;"><td colspan="4" style="text-align: left">Итого осмотрено: </td><td></td><td></td><td></td><td>%d</td></tr>
+        <tr style="height: 300px; border-style: solid;"><td colspan="4" style="text-align: left">Итого прошло послерейсовый: </td><td></td><td></td><td></td><td>%d</td></tr>
+        <tr style="height: 300px; border-style: solid;"><td colspan="4" style="text-align: left">Итого прошло послесменный: </td><td></td><td></td><td></td><td>%d</td></tr>
+        <tr style="height: 300px; border-style: solid;"><td colspan="4" style="text-align: left">Итого не прошло послерейсовый: </td><td></td><td></td><td></td><td>%d</td></tr>
+        <tr style="height: 300px; border-style: solid;"><td colspan="4" style="text-align: left">Итого не прошло послесменный: </td><td></td><td></td><td></td><td>%d</td></tr>`,
+		allCount, goodAfter, goodAftershift, badAfter, badAftershift)
+}
 
